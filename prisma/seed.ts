8b0ce2d9bb0f6db88b5794d16439b08c6e89799f
Wagner Ferreira Costa <wagner.costa@zema.com>
2022-11-03 17:03:54 -0300

    create seedd file

---
+++ prisma/seed.ts
@@ -0,0 +1,31 @@
+import { PrismaClient } from '@prisma/client'
+
+const prisma = new PrismaClient()
+
+async function main() {
+    const user = await prisma.user.create({
+        data: {
+            name: 'John Doe',
+            email: 'john.doe@gmail.com',
+            avatarUrl: 'https://github.com/wagaofc.png',
+        }
+    })
+
+    const pool = await prisma.pool.create({
+        data: {
+            title: 'Example Pool',
+            code: 'BOL123',
+            ownerId: user.id,
+
+            participants: {
+                create: {
+                    userId: user.id
+                }
+            }
+        }
+    })
+
+    
+}
+
+main()
